68de64b515a52563448eda8a8f7fafbf6887cb7e
pranit <patilpranit3112@gmail.com>
2022-06-19 19:41:48 +0530

    Project Setup Completed

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StramChat} from 'stream-chat';
+import {chat} from "stream-chat-react";
+import cookies from 'universal-cookie';
+
+const apiKey='fxbgc58nm5fx';
+
+const App = () => {
+  return (
+    <div>
+        <h1>Realtime Chat Application</h1>
+    </div>
+  )
+}
+
+export default App
